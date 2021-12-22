ca8359f2396e5f1dd11d4ff0955b11d455b2c1ad
didifred <freddy.boutin@gmail.com>
2021-12-22 14:37:49 +0100

    add index member for multi screen touch input event class

--- CustomInputEvents/InputEventMultiScreenTouch.gd
+++ CustomInputEvents/InputEventMultiScreenTouch.gd
@@ -2,11 +2,13 @@ class_name InputEventMultiScreenTouch
 extends InputEventAction
 
 var position
+var index
 
 func _init(e):
 	position = e.position
 	pressed = e.pressed
+	index = e.index
 
 
 func as_text():
-	return "InputEventMultiScreenTouch : position=" + str(position) + ", pressed=" + str(pressed)
+	return "InputEventMultiScreenTouch : position=" + str(position) + ", pressed=" + str(pressed) + ", index=" + str(index)
