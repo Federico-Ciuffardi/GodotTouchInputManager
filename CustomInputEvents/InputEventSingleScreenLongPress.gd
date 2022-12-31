b77b3b3fce3a5d0f1d6d66c0d6741a5c273d2ddd
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2022-12-31 19:17:22 -0300

    Avoid crashing on issue with long press
    * Improve the error log reported on this issue

--- CustomInputEvents/InputEventSingleScreenLongPress.gd
+++ CustomInputEvents/InputEventSingleScreenLongPress.gd
@@ -9,10 +9,9 @@ func _init(_raw_gesture : RawGesture = null) -> void:
 	if raw_gesture:
 		if !raw_gesture.presses.has(0):
 			print("RAW GESTURE:\n" + raw_gesture.as_text())
-			var rollback_res = raw_gesture.rollback_relative(1.0)
-			var discarded_events = rollback_res[1]
+			var linear_event_history = raw_gesture.get_linear_event_history()
 			var history = "\nHISTORY:\n"
-			for e in discarded_events:
+			for e in linear_event_history:
 				if e is RawGesture.Drag:
 					history += "D | "
 				else:
@@ -20,12 +19,10 @@ func _init(_raw_gesture : RawGesture = null) -> void:
 				history += e.as_text()
 				history +="\n"
 			print(history)
-			var error_msg_short = "Hello! we are trying to fix this bug.\nPlease help us by following the steps at the end of the output.\nThanks!"
 			var error_msg="Hello! we are trying to fix this bug.\nTo help us please copy the output and comment it (attached as a file) in the following issue: https://github.com/Federico-Ciuffardi/GodotTouchInputManager/issues/20\nAlso, if you can, include in that comment what version of Godot you are using, what platform you are running on, and what you were doing when the error occurred.\nThanks!"
 			print(error_msg)
 			
-			assert(false,error_msg_short)
-		position = raw_gesture.presses[0].position
+		position = raw_gesture.presses.values()[0].position
 
 
 func as_text() -> String:
